322121a1d152ace3f2d6affced478a7deb73d717
Nayeem Reza <reza.uranium@gmail.com>
2019-04-12 01:17:07 +0600

    Add enquirer and get user email, password

---
+++ src/index.js
@@ -0,0 +1,45 @@
+const { Form } = require('enquirer');
+
+const validateEmail = (email) => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+(async () => {
+  const prompt = new Form({
+    name: 'user',
+    message: 'Please provide your twitter credentials:',
+    choices: [
+      {
+        name: 'email',
+        message: 'Email',
+        initial: 'example@gmail.com',
+        validate(value) {
+          const valid = validateEmail(value);
+          if (!valid) {
+            this.error = 'Invalid email address';
+            return false;
+          }
+          this.error = void 0;
+          return true;
+        },
+      },
+      {
+        name: 'password',
+        message: 'Password',
+        format(input = this.input) {
+          if (!this.keypressed) return '';
+          const color = this.state.submitted
+            ? this.styles.primary
+            : this.styles.muted;
+          return color(this.symbols.asterisk.repeat(input.length));
+        },
+      },
+    ],
+  });
+
+  prompt
+    .run()
+    .then(({ email, password }) => console.log(email, password))
+    .catch(console.error);
+})();
